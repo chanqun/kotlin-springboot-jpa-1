209015d6d303d4cf32d53e9ddcd9e8e8396a7e28
Chanqun Sung <cksgns93@naver.com>
2021-07-26 22:33:00 +0900

    데이터 초기화

--- src/main/kotlin/com/jpabook/jpashop/repository/OrderSearch.kt
+++ src/main/kotlin/com/jpabook/jpashop/repository/OrderSearch.kt
@@ -3,6 +3,6 @@ package com.jpabook.jpashop.repository
 import com.jpabook.jpashop.domain.OrderStatus
 
 class OrderSearch(
-    val memberName: String? = null,
-    val orderStatus: OrderStatus? = null
+    val memberName: String? = "",
+    val orderStatus: OrderStatus? = OrderStatus.ORDER
 )
