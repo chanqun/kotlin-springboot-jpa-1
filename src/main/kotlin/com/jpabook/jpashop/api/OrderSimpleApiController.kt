406b28eb0db148e24b3839915e3058379d24f502
Chanqun Sung <cksgns93@naver.com>
2021-07-28 23:11:48 +0900

    조회 v2

--- src/main/kotlin/com/jpabook/jpashop/api/OrderSimpleApiController.kt
+++ src/main/kotlin/com/jpabook/jpashop/api/OrderSimpleApiController.kt
@@ -1,9 +1,8 @@
 package com.jpabook.jpashop.api
 
+import com.jpabook.jpashop.api.dto.SimpleOrderDto
 import com.jpabook.jpashop.domain.Order
 import com.jpabook.jpashop.repository.OrderRepository
-import com.jpabook.jpashop.repository.OrderRepositoryImpl
-import com.jpabook.jpashop.repository.OrderSearch
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -26,5 +25,15 @@ class OrderSimpleApiController(
         //hibernate 5 module (jackson datatype hibernate5) 쓸 수 있는데 쓰지마라
         //엔티티 노출 ㄴ
         return orderRepository.findAll()
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    fun ordersV2(): List<SimpleOrderDto> {
+        //lazy loading으로 인해 데이터를 너무 많이 조회함 order - delivery - member - address
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N  --> 그렇다고 EAGER을 쓰면 안 된다.
+        return orderRepository.findAll().map {
+            SimpleOrderDto(it.id, it.member!!.name, it.orderDate, it.status, it.delivery!!.address)
+        }
     }
 }
